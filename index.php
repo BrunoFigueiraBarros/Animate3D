be79c1ecfb8b0743756c0e99789edc1ae719172f
BrunoFigueiraBarros <56081466+BrunoFigueiraBarros@users.noreply.github.com>
2023-06-16 21:58:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,6 @@
 	<script src="assets/js/loading.js"></script>
 	<script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
 </head>
-
-
 <body>
 	<div id="loading-container">
 		<div class="progress">
